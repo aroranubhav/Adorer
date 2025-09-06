8533343307ff1f1ffeed2898381f382e33f03de6
maxi <arora.anubhav93@gmail.com>
2025-09-06 21:41:00 +0530

    Add extension methods

--- app/src/main/java/com/maxi/adorer/domain/model/Quote.kt
+++ app/src/main/java/com/maxi/adorer/domain/model/Quote.kt
@@ -1,6 +1,11 @@
 package com.maxi.adorer.domain.model
 
+import com.maxi.adorer.data.source.db.entity.QuotesEntity
+
 data class Quote(
     val id: String,
     val quote: String
 )
+
+fun Quote.toQuoteEntity(): QuotesEntity =
+    QuotesEntity(id, quote)

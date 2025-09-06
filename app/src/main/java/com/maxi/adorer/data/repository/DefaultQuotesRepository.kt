08b60d6bcce9aa917c1ebd28e38e72c368928478
maxi <arora.anubhav93@gmail.com>
2025-09-06 22:45:18 +0530

    Add repository implementation

---
+++ app/src/main/java/com/maxi/adorer/data/repository/DefaultQuotesRepository.kt
@@ -0,0 +1,78 @@
+package com.maxi.adorer.data.repository
+
+import com.maxi.adorer.common.Resource
+import com.maxi.adorer.data.common.safeDbCall
+import com.maxi.adorer.data.common.toQuotesEntityList
+import com.maxi.adorer.data.source.db.dao.QuotesDao
+import com.maxi.adorer.data.source.db.entity.toQuote
+import com.maxi.adorer.domain.model.Quote
+import com.maxi.adorer.domain.model.toQuoteEntity
+import com.maxi.adorer.domain.repository.QuotesRepository
+import com.maxi.adorer.domain.source.filesystem.FileReader
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class DefaultQuotesRepository @Inject constructor(
+    private val quotesDao: QuotesDao,
+    private val sentQuotesDao: QuotesDao,
+    private val fileReader: FileReader
+) : QuotesRepository {
+
+    override suspend fun seedDb(fileName: String): Resource<Unit> {
+        return when (val insertResponse = safeDbCall {
+            val quotes = fileReader.readQuotesFromFile(fileName)
+            quotesDao.insertQuotes(quotes.toQuotesEntityList())
+        }) {
+            is Resource.Success -> {
+                Resource.Success(Unit)
+                //TODO: mark the seeding done!
+            }
+
+            is Resource.DatabaseError -> {
+                Resource.DatabaseError(insertResponse.errorMessage)
+            }
+
+            is Resource.IOError -> {
+                Resource.IOError(insertResponse.errorMessage)
+            }
+
+            is Resource.UnknownError -> {
+                Resource.UnknownError
+            }
+        }
+    }
+
+    /**
+     * The flow starts emitting QuoteEntity objects.
+     * Each emitted QuoteEntity is mapped to Resource.Success(Quote).
+     * Each resulting Resource.Success(Quote) is passed to the collect block one at a time.
+     * Without .collect, the flow is defined but inactive — nothing happens until you collect.
+     */
+    override fun getQuote(): Flow<Resource<Quote>> = flow {
+        safeDbCall {
+            quotesDao.getQuote()
+                .map { quoteEntity -> Resource.Success(quoteEntity.toQuote()) }
+                .collect { resource ->
+                    emit(resource)
+                }
+        }
+    }
+
+
+    override suspend fun insertQuote(quote: Quote): Resource<Unit> {
+        return safeDbCall {
+            quotesDao.insertQuote(quote.toQuoteEntity())
+        }
+    }
+
+    override fun getQuotes(): Flow<Resource<List<Quote>>> = flow {
+        safeDbCall {
+            quotesDao.getQuotes()
+                .map { quotes ->
+                    Resource.Success(quotes.map { it.toQuote() })
+                }
+        }
+    }
+}

0e29a436f885906288aa04e78e73efc3ca8114af
maxi <arora.anubhav93@gmail.com>
2025-09-06 23:04:17 +0530

    Add app datastore implementation

---
+++ app/src/main/java/com/maxi/adorer/data/source/datastore/DefaultAppDateStore.kt
@@ -0,0 +1,29 @@
+package com.maxi.adorer.data.source.datastore
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import com.maxi.adorer.data.common.Constants.DB_SEEDING_DONE
+import com.maxi.adorer.data.common.Constants.DB_SEED_DATA_STORE
+import com.maxi.adorer.domain.source.datastore.AppDatastore
+import kotlinx.coroutines.flow.first
+
+class DefaultAppDateStore(
+    private val context: Context
+) : AppDatastore {
+
+    private val Context.dataStore by preferencesDataStore(name = DB_SEED_DATA_STORE)
+    private val dbSeedingDone = booleanPreferencesKey(DB_SEEDING_DONE)
+
+    override suspend fun checkDbSeedingDone(): Boolean {
+        val prefs = context.dataStore.data.first()
+        return prefs[dbSeedingDone] ?: false
+    }
+
+    override suspend fun markDbSeedingDone() {
+        context.dataStore.edit { prefs ->
+            prefs[dbSeedingDone] = true
+        }
+    }
+}

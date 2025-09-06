3841117d7383ceb3658f7649b9395922ef61fcb1
maxi <arora.anubhav93@gmail.com>
2025-09-06 23:03:59 +0530

    Add constants for data store

--- app/src/main/java/com/maxi/adorer/data/common/Constants.kt
+++ app/src/main/java/com/maxi/adorer/data/common/Constants.kt
@@ -3,4 +3,6 @@ package com.maxi.adorer.data.common
 object Constants {
 
     const val DB_EXCEPTION = "A database exception occurred!"
+    const val DB_SEED_DATA_STORE = "db_seed_ds"
+    const val DB_SEEDING_DONE = "db_seeding_done"
 }

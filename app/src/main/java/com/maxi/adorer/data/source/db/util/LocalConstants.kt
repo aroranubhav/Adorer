9f4a336b880e4ce41ec23092bfbd2396f552868d
maxi <arora.anubhav93@gmail.com>
2025-09-06 18:03:49 +0530

    Add db constants

---
+++ app/src/main/java/com/maxi/adorer/data/source/db/util/LocalConstants.kt
@@ -0,0 +1,18 @@
+package com.maxi.adorer.data.source.db.util
+
+object LocalConstants {
+
+    const val QUOTES_DATABASE = "quotes_db"
+
+    object Tables {
+        const val QUOTES = "quotes"
+        const val SENT_QUOTES = "sent_quotes"
+        const val SENDING_ERRORS = "sending_errors"
+    }
+
+    object Columns {
+        const val ID = "id"
+        const val QUOTE = "quote"
+        const val DATE_TIME = "date_time"
+    }
+}

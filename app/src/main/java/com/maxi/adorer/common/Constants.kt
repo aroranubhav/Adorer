7e663a7fc48fb44b0093b722d0dd8de9899d73c1
maxi <arora.anubhav93@gmail.com>
2025-09-08 12:15:07 +0530

    Add custom actions for sms sent and delivered broadcast

---
+++ app/src/main/java/com/maxi/adorer/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.maxi.adorer.common
+
+object Constants {
+
+    const val SMS_SENT_ACTION = "com.maxi.adorer.SMS_SENT"
+    const val SMS_DELIVERED_ACTION = "com.maxi.adored_SMS_DELIVERED"
+}

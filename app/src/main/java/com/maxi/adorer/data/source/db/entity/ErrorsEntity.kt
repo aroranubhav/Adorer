fef34a7d25974a5a9f15a24afa3a035c1dff67ee
maxi <arora.anubhav93@gmail.com>
2025-09-08 18:30:15 +0530

    Update error entity model

--- app/src/main/java/com/maxi/adorer/data/source/db/entity/ErrorsEntity.kt
+++ app/src/main/java/com/maxi/adorer/data/source/db/entity/ErrorsEntity.kt
@@ -4,17 +4,17 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.maxi.adorer.data.source.db.util.LocalConstants.Columns.DATE_TIME
+import com.maxi.adorer.data.source.db.util.LocalConstants.Columns.ERROR
 import com.maxi.adorer.data.source.db.util.LocalConstants.Columns.ID
-import com.maxi.adorer.data.source.db.util.LocalConstants.Columns.QUOTE
 import com.maxi.adorer.data.source.db.util.LocalConstants.Tables.SENDING_ERRORS
 
 @Entity(SENDING_ERRORS)
 data class ErrorsEntity(
+    @ColumnInfo(ERROR)
+    val error: String,
+    @ColumnInfo(DATE_TIME)
+    val dateTime: String,
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(ID)
-    val id: Int,
-    @ColumnInfo(QUOTE)
-    val quote: String,
-    @ColumnInfo(DATE_TIME)
-    val dateTime: String
+    val id: Int = 0
 )

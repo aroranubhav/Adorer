597b9929e297f99e1b02ed5b9441112dd08ca1b1
maxi <arora.anubhav93@gmail.com>
2025-09-08 18:32:30 +0530

    Add a method to datastore to store the latest quote

--- app/src/main/java/com/maxi/adorer/domain/source/datastore/AppDatastore.kt
+++ app/src/main/java/com/maxi/adorer/domain/source/datastore/AppDatastore.kt
@@ -1,8 +1,14 @@
 package com.maxi.adorer.domain.source.datastore
 
+import com.maxi.adorer.domain.model.Quote
+
 interface AppDatastore {
 
     suspend fun checkDbSeedingDone(): Boolean
 
     suspend fun markDbSeedingDone()
+
+    suspend fun getCurrentQuote(): Quote?
+
+    suspend fun saveCurrentQuote(quote: Quote)
 }

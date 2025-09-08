12f45873d921ee75094e494624cc723d59487b14
maxi <arora.anubhav93@gmail.com>
2025-09-08 14:52:38 +0530

    Add constant value

--- app/src/main/java/com/maxi/adorer/common/Constants.kt
+++ app/src/main/java/com/maxi/adorer/common/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
 
     const val SMS_SENT_ACTION = "com.maxi.adorer.SMS_SENT"
     const val SMS_DELIVERED_ACTION = "com.maxi.adored_SMS_DELIVERED"
+    const val USER_NUMBER = "user_number"
 }

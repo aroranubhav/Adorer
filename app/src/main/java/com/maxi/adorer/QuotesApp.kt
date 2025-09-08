0b39154fec06fb8debd9db5895ff493c32ba74e1
maxi <arora.anubhav93@gmail.com>
2025-09-08 14:52:25 +0530

    Add custom configuration for worker

---
+++ app/src/main/java/com/maxi/adorer/QuotesApp.kt
@@ -0,0 +1,20 @@
+package com.maxi.adorer
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class QuotesApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration
+            .Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}

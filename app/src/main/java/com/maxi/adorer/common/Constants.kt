55b073223eb81691ec62c349bd3ed72d421619d8
maxi <arora.anubhav93@gmail.com>
2025-09-09 11:36:47 +0530

    Add string values and app level constants

--- app/src/main/java/com/maxi/adorer/common/Constants.kt
+++ app/src/main/java/com/maxi/adorer/common/Constants.kt
@@ -2,7 +2,10 @@ package com.maxi.adorer.common
 
 object Constants {
 
+    const val SEED_FILE_NAME = "miamore.json"
     const val SMS_SENT_ACTION = "com.maxi.adorer.SMS_SENT"
     const val SMS_DELIVERED_ACTION = "com.maxi.adored_SMS_DELIVERED"
     const val USER_NUMBER = "user_number"
+    const val MAX_DB_SEEDING_RETRY_COUNT = 3
+    const val GAP_IN_RETRIES = 2000L
 }

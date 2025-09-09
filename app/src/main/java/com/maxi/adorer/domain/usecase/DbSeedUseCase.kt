21f538fc6e67749dc193372e74b4a82cc22faf62
maxi <arora.anubhav93@gmail.com>
2025-09-09 11:46:23 +0530

    Update db seeding to retry if for some reason seeding fails

--- app/src/main/java/com/maxi/adorer/domain/usecase/DbSeedUseCase.kt
+++ app/src/main/java/com/maxi/adorer/domain/usecase/DbSeedUseCase.kt
@@ -1,7 +1,10 @@
 package com.maxi.adorer.domain.usecase
 
+import com.maxi.adorer.common.Constants.GAP_IN_RETRIES
+import com.maxi.adorer.common.Constants.MAX_DB_SEEDING_RETRY_COUNT
 import com.maxi.adorer.common.Resource
 import com.maxi.adorer.domain.repository.QuotesRepository
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class DbSeedUseCase @Inject constructor(
@@ -9,5 +12,20 @@ class DbSeedUseCase @Inject constructor(
 ) {
 
     suspend operator fun invoke(fileName: String): Resource<Unit> =
-        repository.seedDb(fileName)
+        seedWithRetry(fileName)
+
+    private suspend fun seedWithRetry(
+        fileName: String,
+        maxRetries: Int = MAX_DB_SEEDING_RETRY_COUNT,
+        delayMillis: Long = GAP_IN_RETRIES
+    ): Resource<Unit> {
+        repeat(maxRetries - 1) {
+            val result = repository.seedDb(fileName)
+            if (result is Resource.Success) return result
+            delay(delayMillis * (it + 1))
+        }
+        return repository.seedDb(fileName)
+    }
 }
+//TODO: update app to use Kotlin.Result in dao's?,
+// also add a check to see if it fails after max retries to show an error?
